ddfa85a102600050d14c98b96aa1680569401546
BraxB <braxb92@gmail.com>
2021-03-19 19:26:32 -0400

    created loop that color codes hour blocks based on current time

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,5 @@
+
+// clears localStorage and refreshes page, resetting all blocks
 $(".clearBtn").on("click", function(){
     localStorage.clear();
     window.location.reload();
@@ -7,12 +9,28 @@ $(".clearBtn").on("click", function(){
 var currentDay = moment().format("dddd, MMMM Do YYYY");
 $('#currentDay').text(currentDay);
 
+// save buttons will grab the text in the sibling textarea and save it to local storage
 $(".saveBtn").on("click", function(){
     var userText = $(this).siblings("textarea").val();
     var time = $(this).siblings("textarea").attr("id");
     localStorage.setItem(time, userText);
 })
 
+// loop that sets the value of each block to what is in localstorage
 for (i = 9; i < 18; i++) {
     $("#" + i).val(localStorage.getItem(i));
+}
+
+//Create a for loop that colors blocks conditionally by time
+for (i = 9; i < 18; i++) {
+    var currentTime = moment().format("H");
+    if (i < currentTime) {
+        $("#" + i).addClass("past");
+    }
+    else if (i > currentTime) {
+        $("#" + i).addClass("future");
+    }
+    else {
+        $("#" + i).addClass("present");
+    }
 }
