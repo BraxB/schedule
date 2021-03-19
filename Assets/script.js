0c02f584a39fe8ed0dd212735cf26a28e5486bb6
BraxB <braxb92@gmail.com>
2021-03-19 18:28:46 -0400

    created button that allows a user to clear all timeblocks at once

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,10 @@
-var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+$(".clearBtn").on("click", function(){
+    localStorage.clear();
+    window.location.reload();
+})
 
+// set the local date and time in the jumbotron
+var currentDay = moment().format("dddd, MMMM Do YYYY");
 $('#currentDay').text(currentDay);
 
 $(".saveBtn").on("click", function(){
@@ -7,8 +12,6 @@ $(".saveBtn").on("click", function(){
     var time = $(this).siblings("textarea").attr("id");
     localStorage.setItem(time, userText);
 })
-
-$("#9").val(localStorage.getItem("9"));
 
 for (i = 9; i < 18; i++) {
     $("#" + i).val(localStorage.getItem(i));
